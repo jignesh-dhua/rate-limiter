736e33ffd5da09297c5bd0a59da060aae71657a5
Jignesh Dhua <jignesh.dhua1985@gmail.com>
2021-08-09 16:15:01 +0100

    Add rate limiter logic

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
 	fmt.Println("The Rate Limiter")
+
+	requests := make(chan int, 5)
+
+	for i := 1; i <= 5; i++ {
+		requests <- i
+	}
+
+	close(requests)
+
+	limiter := time.NewTicker(time.Second)
+
+	for req := range requests {
+		<-limiter.C
+		fmt.Println("request", req, time.Now())
+	}
 }
